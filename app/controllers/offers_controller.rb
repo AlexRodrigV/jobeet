77ac2179c84ecc2ec5e15c1020203922275ecd81
julien GANTZER <julien.gantzer@epita.fr>
2019-09-14 14:39:02 +0200

    maj des package + première appli de test

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,49 @@
+class OffersController < ApplicationController
+  def index
+   @offer = Offer.all
+ end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+
+    if @offer.save
+      redirect_to @offer
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+
+    if @offer.update(offer_params)
+      redirect_to @offer
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+
+    redirect_to offers_path
+  end
+
+  private
+    def offer_params
+      params.require(:offer).permit(:title, :text)
+    end
+end
