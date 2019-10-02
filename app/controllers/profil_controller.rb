e522597d743bb5576e69fca5c1c3769d97a08056
julien GANTZER <julien.gantzer@epita.fr>
2019-10-02 16:19:01 +0200

    [Profile] Skills are printed and can be modified

--- app/controllers/profil_controller.rb
+++ app/controllers/profil_controller.rb
@@ -4,8 +4,12 @@ class ProfilController < ApplicationController
   end
 
   def edit
-    User.where("email= ?", @var.Email).first.update_columns('description': params[:description], 'hobbies': params[:hobbies], 'company': params[:company], 'street': params[:street], 'city': params[:city], 'state': params[:state], 'resume': params[:resume])
     @currentUser = User.where("email= ?", @var.Email).first
+    userId = @currentUser.id
+    params[:skills].each do |skill|
+      skillId = Skill.where(name: skill).first.id
+      SkillUser.create user_id:userId, skill_id:skillId
+    end
+    User.where("email= ?", @var.Email).first.update_columns('description': params[:description], 'hobbies': params[:hobbies], 'company': params[:company], 'street': params[:street], 'city': params[:city], 'state': params[:state], 'resume': params[:resume])
   end
 end
-  
