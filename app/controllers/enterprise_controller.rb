d4947d5d8f937d82e9845c506aa598788d537e7c
julien GANTZER <julien.gantzer@epita.fr>
2019-10-15 19:27:46 +0200

    work on messanger

--- app/controllers/enterprise_controller.rb
+++ app/controllers/enterprise_controller.rb
@@ -56,27 +56,29 @@ class EnterpriseController < ApplicationController
     @offer = Offer.find(params[:id])
     users = User.where.not(isRecruiter: true, situation: 0)
     @maxPrinted = [10, users.length - Application.where(offer_id: @offer.id).length].min
-    @percentages = Array.new(@maxPrinted, 0)
-    @suggestions = Array.new(@maxPrinted, nil)
-    users.each do |user|
-      if (Application.where(user_id: user.id, offer_id: @offer.id).exists? == false)
-        percentage = getPercentage(@offer.id, user.id)
-        if (percentage >= @percentages.last)
-          @percentages[@maxPrinted-1] = percentage
-          @suggestions[@maxPrinted-1] = user
-          for i in ((@maxPrinted-2).downto(0))
-            if percentage < @percentages[i]
-              @percentages[i+1] = percentage
-              @suggestions[i+1] = user
-              break
-            elsif i == 0
-              @percentages[i+1] = @percentages[i]
-              @suggestions[i+1] = @suggestions[i]
-              @percentages[i] = percentage
-              @suggestions[i] = user
-            else
-              @percentages[i+1] = @percentages[0]
-              @suggestions[i+1] = @suggestions[0]
+    if @maxPrinted > 0
+      @percentages = Array.new(@maxPrinted, 0)
+      @suggestions = Array.new(@maxPrinted, nil)
+      users.each do |user|
+        if (Application.where(user_id: user.id, offer_id: @offer.id).exists? == false)
+          percentage = getPercentage(@offer.id, user.id)
+          if (percentage >= @percentages.last)
+            @percentages[@maxPrinted-1] = percentage
+            @suggestions[@maxPrinted-1] = user
+            for i in ((@maxPrinted-2).downto(0))
+              if percentage < @percentages[i]
+                @percentages[i+1] = percentage
+                @suggestions[i+1] = user
+                break
+              elsif i == 0
+                @percentages[i+1] = @percentages[i]
+                @suggestions[i+1] = @suggestions[i]
+                @percentages[i] = percentage
+                @suggestions[i] = user
+              else
+                @percentages[i+1] = @percentages[0]
+                @suggestions[i+1] = @suggestions[0]
+              end
             end
           end
         end
@@ -126,6 +128,17 @@ class EnterpriseController < ApplicationController
     redirect_to "/enterprise/#{params[:id]}"
   end
 
+  def acceptSuggestion
+    idAcceptedUser = params[:idAcceptSuggestion]
+    tmp = User.find(idAcceptedUser).email.clone.gsub!(/[@.-_]/, '@' => "at", '.' => '', '-' => '', '_' => '').downcase
+    Application.create offer_id: params[:idOffer], user_id: idAcceptedUser, percentage: getPercentage(params[:idOffer], idAcceptedUser), idchatuser: tmp, idchatoffer: Offer.find(params[:idOffer]).identifier, isAccepted: true
+  end
+
+  def acceptApplicant
+    idAcceptedUser = params[:idAcceptApplicant]
+    tmp = User.find(idAcceptedUser).email.clone.gsub!(/[@.-_]/, '@' => "at", '.' => '', '-' => '', '_' => '').downcase
+    Application.where(offer_id: params[:idOffer], user_id: idAcceptedUser).first.update_columns(isAccepted: true)
+  end
 
 end
 end
